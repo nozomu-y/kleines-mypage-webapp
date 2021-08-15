f28bcb9213e50ab037a6e80302f96b8b3dc4a022
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2021-08-15 16:47:47 +0900

    Add: check if the user is authorized

---
+++ app/Http/Middleware/MemberAuth.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class MemberAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!session()->has('jwt_token') || session('jwt_token') == null) {
+            session()->forget('jwt_token');
+            session()->forget('user_id');
+            return redirect()->route('login');
+        }
+        return $next($request);
+    }
+}
