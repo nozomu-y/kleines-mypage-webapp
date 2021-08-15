f94cfc99757b243d6fbc657adad0654eebc60a53
nozoyama39 <nozoyama39@gmail.com>
2021-08-15 16:55:42 +0900

    Update: changed api response data

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    function index(Request $request) {
+        return view('auth.login');
+    }
+
+    function login(Request $request)
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        // call api
+        $body = [
+            'email' => $email,
+            'password' => $password,
+        ];
+        $result = callApi('/auth', 'post', $body, null, false);
+
+        if ($result->code == 200 && $result->data->token != "" && $result->data->token != null) {
+            session()->put('jwt_token', 'Bearer ' . $result->data->token);
+            session()->put('user_id', $result->data->user_id);
+            return redirect()->route('home');
+        }
+
+        return redirect()->route('login')->with([
+            'error' => 'Wrong email or password.'
+        ]);
+    }
+}
